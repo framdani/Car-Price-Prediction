0fe355be2b4b9bb2094191753a49fc58d38eecc5
Fatima-zahra RAMDANI <fatimazahra.ramdani@usmba.ac.ma>
2023-02-05 04:44:48 +0100

    Fine-tuned the model by adjusting the learning rate and epochs

--- predict.py
+++ predict.py
@@ -1,12 +1,13 @@
-from train import estimate_price, normalize_features
+from train import estimate_price, feature_scaling
 import numpy as np
 
 milleage = float(input("Enter the milleage :"))
 data = np.load("min_max_theta.npz")
-min_milleage= data['min']
-max_milleage = data['max']
+min_mileage= data['min']
+max_mileage = data['max']
 theta0 = data['theta0']
 theta1 = data['theta1']
-normalized_milleage = (milleage - min_milleage)/(max_milleage - min_milleage)
-estimatedPrice = estimate_price(normalized_milleage, theta0, theta1)
-print("The estimated price for a car of", milleage,"is",estimatedPrice)
+normalized_milleage = (milleage - min_mileage)/(max_mileage - min_mileage)
+normalizedEstimatedPrice = estimate_price(normalized_milleage, theta0, theta1)
+originalEstimatedPrice = normalizedEstimatedPrice * (max_mileage - min_mileage) + min_mileage
+print("The estimated price for a car of", milleage,"is",originalEstimatedPrice)
--- train.py
+++ train.py
@@ -2,13 +2,15 @@ import csv
 import sys
 import numpy as np
 
-def read_dataset(path):
+def load_dataset(path='data.csv'):
     """
     Reads the dataset from a given file path.
+
     Parameters:
         path(str): The file path of the dataset.
 
     Returns:
+
         milleages(list): the list of milleages.
         prices(list): the list of prices.
     """
@@ -26,11 +28,8 @@ def read_dataset(path):
             for row in reader:
                 milleages.append(float(row[0]))
                 prices.append(float(row[1]))
-    except FileNotFoundError:
-        print("Error: The file", path, "was not found.")
-        sys.exit()
-    except:
-        print("Error: An unknown error occured while reading the file.")
+    except Exception as e:
+        print(f"An exception occured: {e}")
         sys.exit()
     
     print("Dataset read successfully from", path)
@@ -39,26 +38,27 @@ def read_dataset(path):
 def estimate_price(milleage, theta0, theta1):
     return (theta0 + theta1 * milleage)
 
-def cost_function(X, y, theta0, theta1):
+def cost_function(X, Y, theta0, theta1):
     m = len(X)
     sqrd_error = 0
     for i in range(m):
         y_pred = theta0 + theta1 * X[i]
-        sqrd_error += (y_pred - y[i]) ** 2
+        sqrd_error += (y_pred - Y[i]) ** 2
     cost = 1/ (2 * m) * sqrd_error
     return cost
 
 def gradient_descent(milleages, prices, numIterations, learningRate):
     """
     Performs linear regression on a given dataset.
+
     Parameters:
-        milleages(list) : the list of milleages.
+        mileages(list) : the list of mileages.
         prices(list) : the list of prices.
         numIterations(int) : the number of iterations the gradient descent algo will run for.
         learningRate(float) : number of steps the gradient descent algo will take.
     
     Returns:
-        tuple : the estimated values of theta0 and theta1
+        tuple : Estimated values of theta0 and theta1, list of cost value at each iteration.
 
     """
     # Number of elements in the dataset
@@ -66,56 +66,48 @@ def gradient_descent(milleages, prices, numIterations, learningRate):
     # Initial values of theta0 and theta1
     theta0 = 0
     theta1 = 0
-    #X_norm = normalize_features(milleages)
+    costs = []
 
     # performing Gradient descent
-    for j in range(numIterations):
+    for epoch in range(numIterations):
         # Initialize temporary values of theta0 and theta1
         tmp_theta0 = 0
         tmp_theta1 = 0
-
-        costs = []
         # Loop through the dataset
         for i in range(m):
-            # print(prices[i], milleages[i])
             # Estimate the price based on the current values of theta0 and theta1
             estimatedPrice = estimate_price(milleages[i], theta0, theta1)
-            print("estimadPrice",estimatedPrice)
             # Update temporary values of theta0 and theta1
             tmp_theta0 += estimatedPrice - prices[i]
             tmp_theta1 += (estimatedPrice - prices[i]) * milleages[i]
-            print('tmp theta0',tmp_theta0, 'tmp theta1 ',tmp_theta1)
 
         # Update values of theta0 and theta1 based on temporary values
         theta0 = theta0 - learningRate * (1/m) * tmp_theta0
         theta1 = theta1 - learningRate * (1/m) * tmp_theta1
-        costs.append(cost_function(milleages,prices, theta0, theta1))
-    # print(theta0 ,theta1)
+        costs.append(cost_function(milleages, prices, theta0, theta1))
+
     return theta0, theta1, costs
 
-def normalize_features(milleages):
-    # mean_milleages = sum(milleages)/len(milleages)
-    # std_millages = (sum((x - mean_milleages) ** 2 for x in milleages)/ len(milleages))**0.5
-    # milleages_normalized = [(x - mean_milleages)/std_millages for x in milleages]
-    # return milleages_normalized, mean_milleages, std_millages
-    min_milleage = min(milleages)
-    max_milleage = max(milleages)
-    range = max_milleage - min_milleage
-    milleages_normalized = [(x - min_milleage) / range for x in milleages] 
-    return milleages_normalized, min_milleage, max_milleage
+def feature_scaling(data):
+    Min = min(data)
+    Max = max(data)
+    scaling_range = Max - Min
+    normalized_data = [(value - Min) / scaling_range for value in data] 
+    return normalized_data, Min, Max
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Error: Please provide the path to the dataset file.")
-        sys.exit()
-    file_path = sys.argv[1]
-    milleages, prices = read_dataset(file_path)
-    learningRate  = 0.01
-    numIterations = 1000
-    X_norm, min_milleage, max_milleage = normalize_features(milleages)
-    theta0, theta1, costs= gradient_descent(X_norm, prices, numIterations, learningRate)
-    np.savez("min_max_theta.npz", min=min_milleage, max = max_milleage, theta0=theta0, theta1=theta1)
-    print(costs)
+    # Load dataset
+    if len(sys.argv) == 2:
+        mileages, prices = load_dataset(sys.argv[1])
+    else:
+        mileages, prices = load_dataset()
+    # Define hyperparameters
+    learningRate  = 0.0001
+    numIterations = 1000000
+    # Min-Max normalization
+    X_norm, min_mileage, max_mileage = feature_scaling(mileages)
+    Y_norm, min_price, max_price = feature_scaling(prices)
+    theta0, theta1, costs= gradient_descent(X_norm, Y_norm, numIterations, learningRate)
+    np.savez("min_max_theta.npz", min=min_mileage, max = max_mileage, theta0=theta0, theta1=theta1)
 
-
-# 
+    print(costs[-1]) # The min cost

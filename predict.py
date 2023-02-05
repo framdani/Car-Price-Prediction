Fix incorrect calculation of original price

predict.py
@@ -3,11 +3,11 @@ import numpy as np
 
 milleage = float(input("Enter the milleage :"))
 data = np.load("min_max_theta.npz")
-min_mileage= data['min']
-max_mileage = data['max']
+min_mileage= data['min_m']
+max_mileage = data['max_m']
 theta0 = data['theta0']
 theta1 = data['theta1']
 normalized_milleage = (milleage - min_mileage)/(max_mileage - min_mileage)
 normalizedEstimatedPrice = estimate_price(normalized_milleage, theta0, theta1)
-originalEstimatedPrice = normalizedEstimatedPrice * (max_mileage - min_mileage) + min_mileage
-print("The estimated price for a car of", milleage,"is",originalEstimatedPrice)
+originalEstimatedPrice = normalizedEstimatedPrice * (data['max_p'] - data['min_p']) + data['min_p']
+print("The estimated price for a car of", milleage," km is",originalEstimatedPrice)

train.py
@@ -108,6 +108,6 @@ if __name__ == '__main__':
     X_norm, min_mileage, max_mileage = feature_scaling(mileages)
     Y_norm, min_price, max_price = feature_scaling(prices)
     theta0, theta1, costs= gradient_descent(X_norm, Y_norm, numIterations, learningRate)
-    np.savez("min_max_theta.npz", min=min_mileage, max = max_mileage, theta0=theta0, theta1=theta1)
+    np.savez("min_max_theta.npz", min_m=min_mileage, max_m = max_mileage, min_p = min_price, max_p=max_price,theta0=theta0, theta1=theta1)
 
     print(costs[-1]) # The min cost